6a0689c9f760ece8dca6d6005c85ed46ae5041f2
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-01-09 10:30:04 -0500

    feat: Include Badge file into the project

---
+++ src/components/Bagge.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import confLogo from '../images/badge-header.svg';
+
+class Badge extends React.Component {
+    render() {
+      return (
+        <div className="Badge">
+          <div className="Badge__header">
+            <img src={confLogo} alt="Logo de la conferencia" />
+          </div>
+  
+          <div className="Badge__section-name">
+            <img
+              className="Badge__avatar"
+              src="https://www.gravatar.com/avatar?d=identicon"
+              alt="Avatar"
+            />
+            <h1>
+              Jaiden <br /> Riaño
+            </h1>
+          </div>
+  
+          <div className="Badge__section-info">
+            <h3>Desarrollador Master</h3>
+            <div>@jaidenmeiden</div>
+          </div>
+  
+          <div className="Badge__footer">#platziconf</div>
+        </div>
+      );
+    }
+  }
+  
+  export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = 'Jaiden';
-const element = (
-    <div>
-        <h1>Hello, {name}</h1>     
-        <p>Sony ingeniero industrail desarrollador!</p>
-    </div>
-);
+import Badge from './components/Bagge'
+
+
 const container = document.getElementById('root');
 
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge />, container);
