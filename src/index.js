956cf59c7569aa8219b7cad7c479a9120f0356db
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-01-08 23:28:20 -0500

    feat: Undertanding JSX

--- src/index.js
+++ src/index.js
@@ -14,21 +14,59 @@ container.appendChild(element);
 // Utilizando react modificado
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Jaiden 2</h1>;
+//Opción JSX A
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+const jsx1 = <h1>Hello, Jaiden 1</h1>;
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Opción JSX B
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+const name1 = 'Jaiden';
+const jsx2 = <h1>Hello, {name1} 2</h1>;
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Opción JSX C
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+const sum  = () => 3 + 3;
+const jsx3 = <h1>La suma es: {sum()}</h1>;
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Opción JSX D
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+const jsx4 = <h1>Valores nulos: {false}, {null}, {0}, {undefined}</h1>;
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Opción JSX E
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+const jsx5 = (
+    <div>
+        <h1>Hello, {name1}</h1>     
+        <p>Sony ingeniero industrail desarrollador!</p>
+    </div>
+);
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Opción createElement A
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+const element1 = React.createElement('h1', {}, 'Hello, Jaiden 3');
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Opción createElement B
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+const element2 = React.createElement('a', {href: 'https://www.nike.com/cl/'}, 'Hello, Jaiden 4');
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Opción createElement C
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+const name2 = 'Jaiden';
+const element3 = React.createElement('h1', {}, `Hello, ${name2} 5`);
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 const container = document.getElementById('root');
 
-ReactDOM.render(element, container);
-*/
+ReactDOM.render(jsx5, container);
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Utilizando react predeterminado
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++
-
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -41,3 +79,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
