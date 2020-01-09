06bd414f7d6b8a530b675b4416f65ab5820c923e
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-01-09 12:34:19 -0500

    feat: State Lifting

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,29 @@ import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      jobTitle: '',
+      twitter: '',
+    },
+  };
+
+  handleChange = e => {
+    //console.log({
+    //  name: e.target.name,
+    //  value: e.target.value
+    //})
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
@@ -19,17 +42,20 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName='Jaiden'
-                lastName='Riaño'
-                twitter='jaidenmeiden'
-                jobTitle='Industrial Engineer'
-                email='jaidenmeiden@gmail.com'
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
                 avatarUrl="https://s.gravatar.com/avatar/eb283f6be60bc6df15812a9ad7e982e2?s=80"
               />
             </div>
 
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
         </div>
