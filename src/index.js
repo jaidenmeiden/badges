d285c9cedf45812e0b9bd874e32d95518dfff768
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-01-09 11:22:18 -0500

    feat: Import BadgeNew

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
-import Badge from './components/Bagge'
+import BadgeNew from './pages/BadgeNew'
 
 
 const container = document.getElementById('root');
 
-ReactDOM.render(<Badge 
-    firstName= 'Freda' 
-    lastName = 'Grady'
-    avatarUrl= 'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon'
-    email = 'Leann_Berge@gmail.com'
-    jobTitle = 'Legacy Brand Director'
-    twitter = 'jaidenmeiden'/>, container);
+ReactDOM.render(<BadgeNew />, container);
