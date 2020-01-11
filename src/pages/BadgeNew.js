180906e05c59ff19c7e1a9ecc30e2d082a5bbd8b
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-01-10 21:10:56 -0500

    feat: Manage Submit (POST)

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -4,10 +4,13 @@ import './styles/BadgeNew.css';
 import header from '../images/platziconf-logo.svg';
 import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
+import PageLoading from '../components/PageLoading';
 import api from '../api';
 
 class BadgeNew extends React.Component {
   state = {
+    loading: false,
+    error: null,
     form: {
       firstName: '',
       lastName: '',
@@ -18,10 +21,6 @@ class BadgeNew extends React.Component {
   };
 
   handleChange = e => {
-    //console.log({
-    //  name: e.target.name,
-    //  value: e.target.value
-    //})
     this.setState({
       form: {
         ...this.state.form,
@@ -37,12 +36,18 @@ class BadgeNew extends React.Component {
     try {
       await api.badges.create(this.state.form);
       this.setState({ loading: false });
+
+      this.props.history.push('/badges');
     } catch (error) {
       this.setState({ loading: false, error: error });
     }
   };
 
   render() {
+    if (this.state.loading) {
+      return <PageLoading />;
+    }
+
     return (
       <React.Fragment>
         <div className="BadgeNew__hero">
@@ -62,7 +67,7 @@ class BadgeNew extends React.Component {
                 twitter={this.state.form.twitter || 'twitter'}
                 jobTitle={this.state.form.jobTitle || 'JOB_TITLE'}
                 email={this.state.form.email || 'EMAIL'}
-                avatarUrl="https://s.gravatar.com/avatar/eb283f6be60bc6df15812a9ad7e982e2?s=80"
+                avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
               />
             </div>
 
@@ -71,6 +76,7 @@ class BadgeNew extends React.Component {
                 onChange={this.handleChange}
                 onSubmit={this.handleSubmit}
                 formValues={this.state.form}
+                error={this.state.error}
               />
             </div>
           </div>
